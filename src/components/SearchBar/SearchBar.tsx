52388fda30b2c618988d21884e696ce5ec921599
castetus <kboikov@mail.ru>
2023-03-08 12:44:16 +0100

    feat: add css files for components

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { LSKey } from '../../constants';
+import './style.css';
+
+class SearchBar extends React.Component {
+  state = {
+    searchQuery: '',
+  };
+
+  componentDidMount(): void {
+    try {
+      const savedQuery = localStorage.getItem(LSKey) || '';
+      this.setQuery(savedQuery);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  componentWillUnmount(): void {
+    localStorage.setItem(LSKey, this.state.searchQuery);
+  }
+  setQuery(val: string) {
+    this.setState({ searchQuery: val });
+  }
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          className="search-bar-textfield"
+          onChange={(e) => this.setQuery(e.target.value)}
+          type="text"
+          value={this.state.searchQuery}
+        />
+        <span className="search-bar-icon" />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
