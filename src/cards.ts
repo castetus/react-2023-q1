1315e3738db85b2c4cb578ef9e644fab2eb4e900
castetus <kboikov@mail.ru>
2023-03-08 19:31:55 +0100

    add cards & card styles

---
+++ src/cards.ts
@@ -0,0 +1,13 @@
+import { ICard } from '@/components/Card/types';
+
+const names = ['bird', 'boot', 'blouse', 'cat', 'chick', 'chicken', 'coat', 'cry'];
+
+export const cards = names.map((item) => {
+  return {
+    data: {
+      imgSrc: item,
+      title: item,
+      description: `${item} description`,
+    },
+  };
+}) as unknown as ICard[];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import SearchBar from '@/components/SearchBar/SearchBar';
-import { ICard } from '@/components/Card/types';
 import Card from '@/components/Card/Card';
-
-const testCard = {
-  data: {
-    imgSrc: 'boot',
-    title: 'title1',
-    description: 'description1',
-  },
-} as ICard;
+import { cards } from '@/cards';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="page-wrapper">
         <SearchBar />
-        <Card data={testCard.data} />
+        <div className="cards-grid">
+          {cards.map((card, i) => (
+            <Card data={card.data} key={i} />
+          ))}
+        </div>
       </div>
     );
   }
