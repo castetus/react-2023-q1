578fdec28dfc869b36dbc781ee8e7804233e8f80
castetus <kboikov@mail.ru>
2023-03-08 12:43:14 +0100

    feat: add css files for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
