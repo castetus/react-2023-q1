3f4c56d8de5a04f1f24aac74e250cc0c77022f8d
castetus <kboikov@mail.ru>
2023-03-08 20:10:50 +0100

    refactor: rename cards data file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBar from '@/components/SearchBar/SearchBar';
 import Card from '@/components/Card/Card';
-import { cardsData } from '@/cards';
+import { cardsData } from '@/cardsData';
 
 class Home extends React.Component {
   state = {
