d2b6093f84a87df1e14f9197a459fe9ccc9bb6ec
castetus <kboikov@mail.ru>
2023-03-08 16:58:56 +0100

    feat: add card markup

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { ICard } from './types';
 
-class Card extends React.Component {
+class Card extends React.Component<ICard> {
+  constructor(props: ICard) {
+    super(props);
+  }
   render() {
-    return <div className="card"></div>;
+    return (
+      <div className="card">
+        <img src={this.props.imgSrc} alt="" />
+        <span className="card-title">{this.props.title}</span>
+        <span className="card-description">{this.props.description}</span>
+      </div>
+    );
   }
 }
 
