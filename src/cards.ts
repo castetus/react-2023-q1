6caf18010976eaee0bc11d170eb21f6f65b30c53
castetus <kboikov@mail.ru>
2023-03-08 19:43:33 +0100

    refactor: move cardsData to state

--- src/cards.ts
+++ src/cards.ts
@@ -2,7 +2,7 @@ import { ICard } from '@/components/Card/types';
 
 const names = ['bird', 'boot', 'blouse', 'cat', 'chick', 'chicken', 'coat', 'cry'];
 
-export const cards = names.map((item) => {
+export const cardsData = names.map((item) => {
   return {
     data: {
       imgSrc: item,
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import SearchBar from '@/components/SearchBar/SearchBar';
 import Card from '@/components/Card/Card';
-import { cards } from '@/cards';
+import { cardsData } from '@/cards';
 
 class Home extends React.Component {
+  state = {
+    cards: cardsData,
+  };
   render() {
     return (
       <div className="page-wrapper">
         <SearchBar />
         <div className="cards-grid">
-          {cards.map((card, i) => (
+          {this.state.cards.map((card, i) => (
             <Card data={card.data} key={i} />
           ))}
         </div>
