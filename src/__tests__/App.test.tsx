52b24aae4d83a8a4e7503c9c038e3f5f1e8f381f
castetus <kboikov@mail.ru>
2023-03-15 00:12:45 +0100

    add coverage

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import App from '../App';
 import { render, screen } from '@testing-library/react';
 
-test('renders learn react link', () => {
+test('Links exists', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Home/i);
-  expect(linkElement).toBeInTheDocument();
+  const HomeLinkElement = screen.getByText(/Home/i);
+  expect(HomeLinkElement).toBeInTheDocument();
+  const AboutLinkElement = screen.getByText(/About/i);
+  expect(AboutLinkElement).toBeInTheDocument();
 });
