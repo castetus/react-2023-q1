1fc6d7a83b86725425a3107b0f9115006b5c204d
castetus <kboikov@mail.ru>
2023-03-15 00:03:15 +0100

    feat: add testing environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import * as path from 'path';
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,5 @@ export default defineConfig({
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   plugins: [react(), eslint()],
+  // test: vitestConfig.test,
 });
