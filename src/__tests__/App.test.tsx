605d8cb933da95b858c4284b6b2cceff4ac3d7a8
castetus <kboikov@mail.ru>
2023-03-18 17:40:51 +0100

    fix: test for App

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react';
 
 test('Links exists', () => {
   render(<App />);
-  const HomeLinkElement = screen.getByText(/Home/i);
+  const HomeLinkElement = screen.getAllByText(/Home/i)[0];
   expect(HomeLinkElement).toBeInTheDocument();
   const AboutLinkElement = screen.getByText(/About/i);
   expect(AboutLinkElement).toBeInTheDocument();
