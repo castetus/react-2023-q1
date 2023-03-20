e1f1e95628a653dededdc59121e9e323624cf1b6
castetus <kboikov@mail.ru>
2023-03-20 20:12:23 +0100

    feat: add page for form

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '@/layouts/Layout';
 import Home from '@/pages/Home';
 import About from '@/pages/About';
+import Form from '@/pages/Form';
 import NotFound from '@/pages/NotFound';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="form" element={<Form />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
